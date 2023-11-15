1bb79a36f2569ae15ac98ed6d4393c07f16b36e8
Rafli Hada Setiawan <raflijr47@gmail.com>
2023-11-15 11:49:34 +0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toogle Active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkannya
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
